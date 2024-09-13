5c85d71317d645fb5138373fce01a1358b3d3e3f
rooomaisa <romyvandam334@gmail.com>
2024-09-13 12:16:23 +0200

    1 simpel base done

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,60 @@ import './App.css'
 function App() {
 
   return (
-    <>
-        <h1>Fruitmand bezorgservice</h1>
-    </>
+      <>
+          <h1>Fruitmand bezorgservice</h1>
+          <section>
+              <article>
+                  <h2>Aardbeien</h2>
+                  <button type={'button'}>
+                      -
+                  </button>
+                  <button type={'button'}>
+                      +
+                  </button>
+              </article>
+          </section>
+
+          <section>
+              <article>
+                  <h2>Bananen</h2>
+                  <button type={'button'}>
+                      -
+                  </button>
+                  <button type={'button'}>
+                      +
+                  </button>
+              </article>
+          </section>
+
+          <section>
+              <article>
+                  <h2>Appels</h2>
+                  <button type={'button'}>
+                      -
+                  </button>
+                  <button type={'button'}>
+                      +
+                  </button>
+              </article>
+          </section>
+
+          <section>
+              <article>
+                  <h2>Kiwi's</h2>
+                  <button type={'button'}>
+                      -
+                  </button>
+                  <button type={'button'}>
+                      +
+                  </button>
+              </article>
+
+              <button type={"button"}>
+                  Reset
+              </button>
+          </section>
+      </>
   )
 }
 
