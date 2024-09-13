5a5da3d5ca4b15eee15dc715378126e20af4498b
rooomaisa <romyvandam334@gmail.com>
2024-09-13 15:06:59 +0200

    finished 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,83 @@
 
 import './App.css'
+import {useState} from "react";
+// import {useForm} from "react-hook-form";
 
 function App() {
 
-  return (
+    // const {register = useForm()}
+    const [strawberries, setStrawberries] = useState(0)
+    const [bananas, setBananas]= useState(0)
+    const [appels, setAppels] = useState(0)
+    const [kiwis, setKiwis] = useState (0)
+
+
+    function resetFruits() {
+        setKiwis(0);
+        setAppels(0);
+        setBananas(0);
+        setStrawberries(0);
+    }
+
+    return (
       <>
           <h1>Fruitmand bezorgservice</h1>
-          <section>
+          <section className={'fruitbox'}>
               <article>
+                  <span className="icon">🍓</span>
                   <h2>Aardbeien</h2>
-                  <button type={'button'}>
+                  <button type={'button'} onClick={() => setStrawberries(strawberries -1)} disabled={strawberries===0}>
                       -
                   </button>
-                  <button type={'button'}>
+                  {strawberries}
+                  <button type={'button'} onClick={() => setStrawberries(+1)}>
                       +
                   </button>
               </article>
           </section>
 
-          <section>
+          <section className={'fruitbox'}>
               <article>
+                  <span className="icon">🍌</span>
                   <h2>Bananen</h2>
-                  <button type={'button'}>
+                  <button type={'button'} onClick={() => setBananas (bananas -1)} disabled={bananas===0}>
                       -
                   </button>
-                  <button type={'button'}>
+                  {bananas}
+                  <button type={'button'} onClick={() => setBananas (bananas +1)}>
                       +
                   </button>
               </article>
           </section>
 
-          <section>
+          <section className={'fruitbox'}>
               <article>
+                  <span className="icon">🍏</span>
                   <h2>Appels</h2>
-                  <button type={'button'}>
+                  <button type={'button'} onClick={() => setAppels(appels-1)} disabled={appels===0}>
                       -
                   </button>
-                  <button type={'button'}>
+                  {appels}
+                  <button type={'button'} onClick={() => setAppels(appels+1)} >
                       +
                   </button>
               </article>
           </section>
 
-          <section>
+          <section className={'fruitbox'}>
               <article>
+                  <span className="icon">🥝</span>
                   <h2>Kiwi's</h2>
-                  <button type={'button'}>
+                  <button type={'button'} onClick={() => setKiwis(kiwis-1)} disabled={kiwis===0}>
                       -
                   </button>
-                  <button type={'button'}>
+                  {kiwis}
+                  <button type={'button'} onClick={() => setKiwis(kiwis+1)}>
                       +
                   </button>
               </article>
 
-              <button type={"button"}>
+              <button type={"button"} onClick={() => resetFruits()}>
                   Reset
               </button>
           </section>
